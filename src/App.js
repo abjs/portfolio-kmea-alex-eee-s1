2aa62ca51c235511099899fc13900c51730995df
Abin Josephh <itsmeabjs@gmail.com>
2021-05-23 13:53:33 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import './App.css'
+import mypic from './me.jpg'
+import kmea from './kmea.jpg'
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>I AM ALEX SEBASTIAN</h1>
+      <h2>About Me</h2>
+      <p>BTECH student @KMEA Engineering College</p>
+      <img src={mypic} alt="itzme" />
+      <h3>EDUCATION</h3>
+      <br/>
+      <p>
+        I completed my high school education at Little Flower High School,
+        Panayikulam. I pursued my higher secondary education in science stream
+        from GHSS N. Edappally. Currently I am pursuing my degree in Electrical
+        and Electronics Engineering from KMEA Engineering College Edathala,
+        Aluva.
+      </p>
+      <img src={kmea} alt="College" />
+      <h4>ACHIEVEMENTS</h4>
+      <p>
+        •Member of hacklub •Participated in State Zonal Handball Competition
+        •Medalled in sub district level Triple jump competition
+      </p>
+      <h5>HOBBIES</h5>
+      <footer>♤TRAVELLING♤ ●PLAYING FOOTBALL● </footer>
     </div>
   );
 }
-
-export default App;
